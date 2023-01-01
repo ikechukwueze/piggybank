e2905116d7b63ac7698f0dba7d2c9a5e722543d4
ikechukwueze <themedric@live.com>
2023-01-02 00:23:36 +0100

    addeed accounts model

--- account-management-service/accounts/models.py
+++ account-management-service/accounts/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils import timezone
+from django.core.validators import MinLengthValidator
+
+
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
+    last_name = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=10, validators=[MinLengthValidator(10)])
+    bvn = models.CharField(max_length=11, validators=[MinLengthValidator(11)])
+    signup_date = models.DateTimeField(default=timezone.now)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    
