8d005f8f6a37b854fe60a5a455e4349b804defea
ikechukwueze <themedric@live.com>
2023-01-03 01:08:22 +0100

    added custom numeric string validator

--- account-management-service/accounts/custom_validators.py
+++ account-management-service/accounts/custom_validators.py
@@ -2,10 +2,10 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 
-def bvn_string_validator(value: str):
-    if not value.isnumeric:
+
+def numeric_string_validator(value: str):
+    if not value.isnumeric():
         raise ValidationError(
-            _("%(value)s is not a valid bvn"),
+            _("%(value)s is invalid. It should contain only digits"),
             params={"value": value},
         )
-    
