2870b4075254e849b1a6b846d6e7777f2ba5728b
ikechukwueze <themedric@live.com>
2023-01-04 00:02:50 +0100

    test model field max lengths

--- account-management-service/accounts/test/test_models.py
+++ account-management-service/accounts/test/test_models.py
@@ -1,7 +1,32 @@
 from django.test import TestCase
 from django.db.utils import IntegrityError
+from django.core.exceptions import ValidationError
 from accounts.models import Account
 
 
-
 # Create your tests here.
+
+
+class ModelTest(TestCase):
+    def setUp(self) -> None:
+        self.account_details = {
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "johndoe@email.com",
+            "phone_number": "08123456789",
+            "password": "easypassword",
+        }
+
+        self.field_max_lengths = {
+            "first_name": 50,
+            "last_name": 50,
+            "email": 255,
+            "phone_number": 11,
+            "bvn": 11,
+        }
+
+    def test_model_field_max_length(self):
+        for field, max_length in self.field_max_lengths.items():
+            self.assertEquals(Account._meta.get_field(field).max_length, max_length)
+
+    
