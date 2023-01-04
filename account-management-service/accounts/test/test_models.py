3d599f9f3e4fde38f9a2e18e72552e8b17e55838
ikechukwueze <themedric@live.com>
2023-01-05 00:29:08 +0100

    test min length validator on last name field

--- account-management-service/accounts/test/test_models.py
+++ account-management-service/accounts/test/test_models.py
@@ -48,3 +48,8 @@ class ModelTest(TestCase):
         self.account_details["first_name"] = "J"
         with self.assertRaisesMessage(ValidationError, "First name should have at least 2 letters"):
             Account.objects.create(**self.account_details)
+    
+    def test_last_name_with_invalid_min_length_raises_exception(self):
+        self.account_details["last_name"] = "J"
+        with self.assertRaisesMessage(ValidationError, "Last name should have at least 2 letters"):
+            Account.objects.create(**self.account_details)
