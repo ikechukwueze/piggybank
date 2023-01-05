fa5f195e884d76b2947870c4cf1542bb78fcc177
ikechukwueze <themedric@live.com>
2023-01-05 20:36:34 +0100

    add knox token authentication as defualt authentication class in rest_framework settings

--- account-management-service/projectconfig/settings/base.py
+++ account-management-service/projectconfig/settings/base.py
@@ -130,3 +130,9 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.Account"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        "knox.auth.TokenAuthentication",
+    ]
+}
