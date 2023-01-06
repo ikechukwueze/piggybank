29853966f3001fab93273d3d7291c0ca469cdf3a
ikechukwueze <themedric@live.com>
2023-01-06 14:46:03 +0100

    added email field validator in signup serializer

--- account-management-service/accounts/serializers.py
+++ account-management-service/accounts/serializers.py
@@ -1,6 +1,6 @@
+from typing import Union
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
 from .custom_validators import numeric_string_validator
 from .models import Account
 
@@ -8,14 +8,15 @@ from .models import Account
 class SignUpSerializer(serializers.Serializer):
     first_name = serializers.CharField(min_length=2, max_length=50, required=True)
     last_name = serializers.CharField(min_length=2, max_length=50, required=True)
-    email = serializers.EmailField(
-        max_length=255,
-        required=True,
-        validators=[UniqueValidator(queryset=Account.objects.all())],
-    )
+    email = serializers.EmailField(max_length=255, required=True)
     phone_number = serializers.CharField(min_length=11, max_length=11, required=True)
     password = serializers.CharField(required=True, min_length=8, max_length=32)
     confirm_password = serializers.CharField(required=True)
+
+    def validate_email(self, value: str) -> Union[serializers.ValidationError, str]:
+        if Account.objects.filter(email=value).exists():
+            raise serializers.ValidationError("An account with this email already exists.")
+        return value
 
     def validate_phone_number(self, value: str) -> str:
         numeric_string_validator(
