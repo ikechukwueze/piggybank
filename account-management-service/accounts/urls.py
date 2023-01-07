d89de40671052cd18f8c80339a6e9aa5280abcbd
ikechukwueze <themedric@live.com>
2023-01-07 12:37:39 +0100

    add knox logout urls

--- account-management-service/accounts/urls.py
+++ account-management-service/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from knox import views as knox_views
 from . import views
 
 
@@ -6,4 +7,6 @@ from . import views
 urlpatterns = [
     path("signup/", views.AccountSignUpView.as_view(), name="account_signup"),
     path("login/", views.AccountLoginView.as_view(), name="account_login"),
+    path("logout/", knox_views.LogoutView.as_view(), name='knox_logout'),
+    path("logoutall/", knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
 ]
