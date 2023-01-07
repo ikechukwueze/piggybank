cd6b27d70fab715f57029205b67db86b0be8c6c4
ikechukwueze <themedric@live.com>
2023-01-07 12:35:40 +0100

    create account login view

--- account-management-service/accounts/views.py
+++ account-management-service/accounts/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SignUpSerializer
+from .serializers import LoginSerializer
+from .custom_exceptions import MaximumTokensExceeded
 
 # Create your views here.
 
@@ -21,3 +23,28 @@ class AccountSignUpView(APIView):
             "token": token
         }
         return Response(account_details, status=status.HTTP_200_OK)
+
+
+class AccountLoginView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        account = serializer.validated_data["account"]
+        try:
+            token = account.get_token()
+        except MaximumTokensExceeded:
+            return Response(
+                {"error": "Maximum amount of tokens allowed per user exceeded."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        else:
+            account_details = {
+                "first_name": account.first_name,
+                "last_name": account.last_name,
+                "email": account.email,
+                "token": token
+            }
+            return Response(account_details, status=status.HTTP_200_OK)
