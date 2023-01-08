aa69a9af89a4fe112a18392a3bcb2d8635658338
ikechukwueze <themedric@live.com>
2023-01-08 18:54:55 +0100

    delete unused modules

--- account-management-service/accounts/custom_exceptions.py
+++
@@ -1,3 +0,0 @@
-class MaximumTokensExceeded(Exception):
-    "Raised when the maximum number of tokens per user is exceeded."
-    pass
--- account-management-service/accounts/custom_validators.py
+++
@@ -1,8 +0,0 @@
-from typing import Callable, Union
-
-
-def numeric_string_validator(
-    field: str, value: str, ValidationException: Callable, error_msg: Union[str, dict]
-):
-    if value and not value.isnumeric():
-        raise ValidationException(error_msg)
