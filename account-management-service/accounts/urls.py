143894300ea336d21fcb835b887daf9b3b3a10ab
ikechukwueze <themedric@live.com>
2023-01-09 16:27:05 +0100

    added change-password url

--- account-management-service/accounts/urls.py
+++ account-management-service/accounts/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path("signup/", views.AccountSignUpView.as_view(), name="account_signup"),
     path("login/", views.AccountLoginView.as_view(), name="account_login"),
+    path("change-password/", views.ChangePasswordView.as_view(), name="account_change_password"),
     path("logout/", knox_views.LogoutView.as_view(), name='knox_logout'),
     path("logoutall/", knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
 ]
