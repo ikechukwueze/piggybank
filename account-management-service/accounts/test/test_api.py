cb8faac7b37b395e1aecf0c5e15e411ecbdd6ba8
ikechukwueze <themedric@live.com>
2023-01-10 10:20:27 +0100

    test account login

--- account-management-service/accounts/test/test_api.py
+++ account-management-service/accounts/test/test_api.py
@@ -7,16 +7,19 @@ from knox import crypto
 
 
 class APITest(APITestCase):
+    fixtures = ["accounts.json"]
+
     def setUp(self) -> None:
         self.account_details = {
-            "first_name": "Jane",
+            "first_name": "John",
             "last_name": "Doe",
-            "email": "janedoe@mail.com",
-            "phone_number": "12345678901",
+            "email": "johndoe@mail.com",
+            "phone_number": "12345678905",
             "password": "fakepassword",
             "confirm_password": "fakepassword",
         }
         self.account_signup_url = reverse("account_signup")
+        self.account_login_url = reverse("account_login")
 
     def test_account_signup(self):
         response = self.client.post(
@@ -29,14 +32,10 @@ class APITest(APITestCase):
         self.assertEqual(
             response.data["first_name"], self.account_details["first_name"]
         )
-        self.assertEqual(
-            response.data["last_name"], self.account_details["last_name"]
-        )
-        self.assertEqual(
-            response.data["email"], self.account_details["email"]
-        )
+        self.assertEqual(response.data["last_name"], self.account_details["last_name"])
+        self.assertEqual(response.data["email"], self.account_details["email"])
         self.assertNotEqual(response.data["token"], "")
-        self.assertEqual(Account.objects.count(), 1)
+        self.assertEqual(Account.objects.count(), 2)
 
     def test_account_signup_token(self):
         response = self.client.post(
@@ -48,3 +47,12 @@ class APITest(APITestCase):
         hashed_token = AuthToken.objects.get(user=account).digest
         self.assertEqual(hashed_token, crypto.hash_token(response_token))
 
+    def test_account_login(self):
+        login_details = {"email": "janedoe@mail.com", "password": "fakepassword"}
+        response = self.client.post(
+            self.account_login_url, login_details, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["email"], login_details["email"])
+        self.assertNotEqual(response.data["token"], "")
+        self.assertTrue(AuthToken.objects.filter(user__email="janedoe@mail.com").exists())
