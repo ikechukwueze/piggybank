02cb526de46cce1956a20738d1d0d3bd8bc3656b
ikechukwueze <themedric@live.com>
2023-02-01 23:51:30 +0000

    create forgot password view

--- account-management-service/accounts/views.py
+++ account-management-service/accounts/views.py
@@ -2,13 +2,22 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import (
+    smart_bytes,
+)
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
 from .serializers import (
-    SignUpSerializer, 
-    LoginSerializer, 
-    ChangePasswordSerializer, 
-    UpdateBvnSerializer
+    SignUpSerializer,
+    LoginSerializer,
+    ChangePasswordSerializer,
+    UpdateBvnSerializer,
+    ForgotPasswordSerializer,
 )
 from utils.exceptions import MaximumTokensExceeded
+from .models import Account
 
 # Create your views here.
 
@@ -24,7 +33,7 @@ class AccountSignUpView(APIView):
             "first_name": account.first_name,
             "last_name": account.last_name,
             "email": account.email,
-            "token": token
+            "token": token,
         }
         return Response(account_details, status=status.HTTP_201_CREATED)
 
@@ -49,7 +58,7 @@ class AccountLoginView(APIView):
                 "first_name": account.first_name,
                 "last_name": account.last_name,
                 "email": account.email,
-                "token": token
+                "token": token,
             }
             return Response(account_details, status=status.HTTP_200_OK)
 
@@ -67,6 +76,32 @@ class ChangePasswordView(APIView):
         return Response(
             {"message": "Password changed successfully"},
             status=status.HTTP_202_ACCEPTED,
+        )
+
+
+class ForgotPasswordView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+        serializer = ForgotPasswordSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        email = serializer.data
+        account = Account.objects.get(email=email)
+        encoded_account_id = urlsafe_base64_encode(smart_bytes(account.id))
+        password_reset_token = PasswordResetTokenGenerator().make_token(account)
+        current_site = get_current_site(request=request).domain
+        relative_link = reverse(
+            "email-password-reset",
+            kwargs={
+                "encoded-account-id": encoded_account_id,
+                "password-reset-token": password_reset_token,
+            },
+        )
+        absolute_url = "http://" + current_site + relative_link
+        print(absolute_url)
+        return Response(
+            {"message": "Password reset email sent."}, status=status.HTTP_200_OK
         )
 
 
