f98511561a0178705f4ac5fa413ab0704ea347f6
ikechukwueze <themedric@live.com>
2023-02-16 23:07:54 +0000

    create currency model

--- wallet-service/wallets/models.py
+++ wallet-service/wallets/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+class WalletCurrency(models.Model):
+    currency = models.CharField(max_length=50)
+    currency_iso = models.CharField(max_length=5)
+
+    def __str__(self) -> str:
+        return self.currency_iso
