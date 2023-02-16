b2a0267b8b8bcb39ddecb7575057d037c1bab2d2
ikechukwueze <themedric@live.com>
2023-02-16 23:15:29 +0000

    create wallet model

--- wallet-service/wallets/models.py
+++ wallet-service/wallets/models.py
@@ -9,3 +9,21 @@ class WalletCurrency(models.Model):
 
     def __str__(self) -> str:
         return self.currency_iso
+
+
+class Wallet(models.Model):
+    class WalletStatus(models.TextChoices):
+        active = 'ACTIVE', 'ACTIVE'
+        inactive = 'INACTIVE', 'INACTIVE'
+        pnd = 'PND', 'PND'
+
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    owner = models.UUIDField()
+    currency = models.ForeignKey(WalletCurrency, on_delete=models.SET_NULL, null=True)
+    balance = models.DecimalField(max_digits=11, decimal_places=2)
+    status = models.CharField(choices=WalletStatus.choices, max_length=8)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return str(self.owner)
