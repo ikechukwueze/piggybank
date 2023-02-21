6ea5395e44800e67d81708673d62096bd1ffb8dc
ikechukwueze <themedric@live.com>
2023-02-21 23:49:42 +0000

    wip

--- account-service/accounts/urls.py
+++ account-service/accounts/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("change-password/", views.ChangePasswordView.as_view(), name="change_account_password"),
     path("request-password-reset/", views.RequestPasswordResetView.as_view(), name="request-password-reset"),
     path("password-reset/<encrypted_user_id>/<password_reset_token>/", views.PasswordResetView.as_view(), name="password-reset"),
+    path("complete-password-reset/<encrypted_user_id>/<password_reset_token>/<encrypted_email/", views.CompletePasswordResetView.as_view(), name="confirm-password-reset"),
     path("update-bvn/", views.UpdateBvnView.as_view(), name="update_bvn"),
     path("logout/", knox_views.LogoutView.as_view(), name='knox_logout'),
     path("logoutall/", knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
