debbeef3a4cbf435ffd17178916f30d2bbc5af9b
ikechukwueze <themedric@live.com>
2023-02-26 23:55:10 +0000

    retrieve wallet balance

--- wallet-service/wallets/views.py
+++ wallet-service/wallets/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from .serializers import WalletBalanceSerializer
+from .models import NairaWallet
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+
+
+class RetrieveWalletBalance(generics.RetrieveAPIView):
+    permission_classes = [IsOwnerOrReadOnly, IsAuthenticated]
+    authentication_classes = []
+    serializer_class = WalletBalanceSerializer
+    queryset = NairaWallet.objects.all()
+
+    def get_object(self):
+        user = self.request.user
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset, **filter)
+        self.check_object_permissions(self.request, obj)
+        return obj
